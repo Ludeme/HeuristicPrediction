a786f90b671cabe88e0af33b750d7aad877e54b0
Matthew Stephenson <matthew.stephenson@maastrichtuniversity.nl>
2021-05-26 14:48:02 +0200

    Fixed random_state to 0 for all valid learning models

--- Models/LearningModels.py
+++ Models/LearningModels.py
@@ -4,14 +4,14 @@ from sklearn.dummy import DummyRegressor
 # Learning models (Regression)
 regressionModels = [
     DummyRegressor(strategy="mean"),
-    linear_model.Ridge(),
-    linear_model.Lasso(),
-    linear_model.ElasticNet(),
+    linear_model.Ridge(random_state=0),
+    linear_model.Lasso(random_state=0),
+    linear_model.ElasticNet(random_state=0),
     linear_model.BayesianRidge(),
-    ensemble.RandomForestRegressor(),
-    ensemble.GradientBoostingRegressor(),
-    tree.DecisionTreeRegressor(),
+    ensemble.RandomForestRegressor(random_state=0),
+    ensemble.GradientBoostingRegressor(random_state=0),
+    tree.DecisionTreeRegressor(random_state=0),
     neighbors.KNeighborsRegressor(),
     svm.LinearSVR(random_state=0),
-    neural_network.MLPRegressor(),
+    neural_network.MLPRegressor(random_state=0),
 ]
